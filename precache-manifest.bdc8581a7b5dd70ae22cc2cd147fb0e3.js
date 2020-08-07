5ded86e84789447fa656c717a6f990cf240703d5
Gonzalo Perelstein <36910726+gonmp@users.noreply.github.com>
2020-08-07 15:05:38 -0300

    Updates

---
+++ precache-manifest.bdc8581a7b5dd70ae22cc2cd147fb0e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a20e43e1fb054da8d2db39e00a3754",
+    "url": "/SoccerMatchesCliente/index.html"
+  },
+  {
+    "revision": "e536b63735ae8258aacf",
+    "url": "/SoccerMatchesCliente/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f72f4f55e09f5cf5bc19",
+    "url": "/SoccerMatchesCliente/static/css/main.74c20fbd.chunk.css"
+  },
+  {
+    "revision": "e536b63735ae8258aacf",
+    "url": "/SoccerMatchesCliente/static/js/2.c140c881.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/SoccerMatchesCliente/static/js/2.c140c881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72f4f55e09f5cf5bc19",
+    "url": "/SoccerMatchesCliente/static/js/main.8e68bc34.chunk.js"
+  },
+  {
+    "revision": "9e30e029d1c49e571ef9",
+    "url": "/SoccerMatchesCliente/static/js/runtime-main.351ca645.js"
+  }
+]);
